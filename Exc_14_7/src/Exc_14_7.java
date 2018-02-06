26353e0202978b4a9bf06a9f49c014f05ea6eb2b
Austyn <Austyn@LAPTOP-M1DPDM1L>
2018-02-06 17:45:16 -0600

    Exercise 14.7

--- Exc_14_7/src/Exc_14_7.java
+++ Exc_14_7/src/Exc_14_7.java
@@ -25,12 +25,14 @@ public class Exc_14_7 extends Application {
         int hor = 0;
         int check = 1; 
         
+        //Create the GridPane and its properties
         GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
         pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.4));
         pane.setHgap(2);
         pane.setVgap(2);
         
+        //Create the text boxes in the grid pane using a loop to create the 10 by 10 and adding a random 0 or 1.
         while (check == 1) {
             int rNum =(Math.random()<0.5)?0:1;
             pane.add(new TextField(Integer.toString(rNum)), ver, hor);
@@ -44,6 +46,7 @@ public class Exc_14_7 extends Application {
             }
         }
         
+        //Add and display the scene
         Scene scene = new Scene(pane, 500, 360);
         
         primaryStage.setTitle("Exc_14.7");
